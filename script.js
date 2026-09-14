6fc6f039d77d6d8244651ebc0ded90937eff4cda
Jonas Karsten <jonas.m.karsten@gmail.com>
2026-09-14 18:41:42 +0200

    Add team contact details and update LinkedIn link

--- script.js
+++ script.js
@@ -21,7 +21,7 @@
             <li><a href="impressum.html">Impressum</a></li>
             <li><a href="privacy-policy.html">Privacy Policy</a></li>
             <li>
-              <a class="linkedin-link" href="https://www.linkedin.com/company/datipilot-bioblock/posts/?feedView=all" target="_blank" rel="noopener">
+              <a class="linkedin-link" href="https://www.linkedin.com/company/bio-block/" target="_blank" rel="noopener">
                 <svg viewBox="0 0 24 24" aria-hidden="true"><path d="M20.5 20.5h-3.6V15c0-1.3 0-3-1.9-3s-2.1 1.4-2.1 2.9v5.6H9.3V9h3.4v1.6h.1c.5-.9 1.6-1.9 3.4-1.9 3.6 0 4.3 2.4 4.3 5.5v6.3ZM5.3 7.4A2.1 2.1 0 1 1 5.3 3a2.1 2.1 0 0 1 0 4.4Zm1.8 13.1H3.5V9h3.6v11.5Z"/></svg>
                 <span>LinkedIn</span>
               </a>
